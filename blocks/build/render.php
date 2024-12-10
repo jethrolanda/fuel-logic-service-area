71ef8b3735752c0d218ec9d2be7262787b0b84f1
Jethro Landa <jemaq_lan@hotmail.com>
2024-12-11 00:03:27 +0800

    style

--- blocks/build/render.php
+++ blocks/build/render.php
@@ -51,15 +51,14 @@ if (isset($attributes['bannedMessagePattern'])) {
     data-wp-init="callbacks.onLoad"
     <?php echo wp_interactivity_data_wp_context($context); ?>
     <?php echo get_block_wrapper_attributes(); ?>>
-    <div class="flex gap-4 justify-center items-center">
+    <div class="flex gap-4 justify-center items-center mb-6">
       <label for="zip-code">Zip Code:</label>
-      <input id="zip-code" class="rounded-md bg-white py-2 px-4 text-base text-gray-900 " data-wp-bind--value="context.zipcode" value="<?php echo $context['zipcode'] > 0 ? $context['zipcode'] : ''; ?>" type="number" min="1" step="1" data-wp-on--keyup="callbacks.setZipcode">
-      <button data-wp-on--click="actions.submit" class="bg-lime-400 p-2 text-black rounded-md text-md">Submit</button>
-      <button class="text-sm" data-wp-on--click="actions.clear">Clear</button>
+      <input id="zip-code" class="border-2 border-lime-400 rounded-md bg-white py-2 px-4 text-base text-gray-900 " data-wp-bind--value="context.zipcode" value="<?php echo $context['zipcode'] > 0 ? $context['zipcode'] : ''; ?>" type="number" min="1" step="1" data-wp-on--keyup="callbacks.setZipcode">
+      <button data-wp-on--click="actions.submit" class="bg-lime-400 py-2 px-6 text-black rounded-md text-md">Submit</button>
+      <button class="text-sm text-red-600" data-wp-on--click="actions.clear">Clear</button>
     </div>
 
     <div data-wp-init--log="context.showMessages" data-wp-bind--hidden="!context.showMessages">
-      <!-- <button data-wp-bind--hidden="!state.hasResult" data-wp-on--click="actions.goBack">Back</button> -->
       <div data-wp-bind--hidden="!context.zipfound">
         <?php echo do_blocks(get_post_field('post_content', $successMessagePattern)); ?>
       </div>
